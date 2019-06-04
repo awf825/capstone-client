c926694f1ad54208ca1420044f60c1571257baf1
awf825 <faiden454@gmail.com>
2019-06-04 12:43:44 -0400

    trying to fix deployment issue in src/apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://pure-sea-43931.herokuapp.com/',
+  production: 'https://pure-sea-43931.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
